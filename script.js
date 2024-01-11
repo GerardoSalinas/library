0f29c4117d31ebb48da5c92601705fead7e3026d
GerardoSalinas <gerardoaplicano16@gmail.com>
2024-01-11 12:05:00 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function addBookToLibrary () {
+
+}
+
+function showBooks () {
+    myLibrary.forEach(function (value,index) {
+        let card = document.createElement('div');
+        
+        //crear un div
+        //darle los estilos
+        //agregar el contenido del libro
+    })
+}
+
